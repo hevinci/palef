347c917284ef381a7a15d07bb87f8d446a476307
Stéphane Klein <stephaneklein221@gmail.com>
2014-10-07 09:41:09 +0200

    Update manifest when app is rebuilt

--- server/build/builder.js
+++ server/build/builder.js
@@ -2,14 +2,18 @@ var fs = require('fs');
 var dotBuilder = require('./dot');
 var stylusBuilder = require('./stylus');
 var browserifyBuilder = require('./browserify');
-var outputPath = __dirname + '/../../public/index.html';
+var indexPath = __dirname + '/../../public/index.html';
+var manifestPath = __dirname + '/../../public/manifest.appcache';
 
 // build the application index and its dependencies
-// (scripts, templates, css)
+// (scripts, templates, css, manifest)
 dotBuilder.compile(function (templates) {
   stylusBuilder.compile(function (stylesheet) {
     browserifyBuilder.compile(function (scripts) {
-      fs.writeFileSync(outputPath, templates.index({
+      fs.writeFileSync(manifestPath, templates.manifest({
+        version: new Date().toString()
+      }));
+      fs.writeFileSync(indexPath, templates.index({
         stylesheet: stylesheet,
         templates: templates.fragments,
         scripts: scripts
--- server/build/dot.js
+++ server/build/dot.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var dot = require('dot');
 var glob = require('glob');
 var indexTemplate = __dirname + '/../template/index.dot';
+var manifestTemplate = __dirname + '/../template/manifest.dot';
 var templateDir = __dirname + '/../../client/template';
 
 /**
@@ -9,6 +10,7 @@ var templateDir = __dirname + '/../../client/template';
  * an object containing:
  *   1) the main index template (function)
  *   2) the template fragments (serialized object)
+ *   3) the application cache manifest (function)
  */
 module.exports.compile = function (callback) {
   glob('**/*.dot', { cwd: templateDir }, function (err, files) {
@@ -23,10 +25,12 @@ module.exports.compile = function (callback) {
     });
     dot.templateSettings.strip = false;
     var index = dot.template(fs.readFileSync(indexTemplate, 'utf8'));
+    var manifest = dot.template(fs.readFileSync(manifestTemplate, 'utf8'));
     dot.templateSettings.strip = true;
     callback({
       index: index,
-      fragments: "{\n" + compiled.join(",\n") + '};'
+      fragments: "{\n" + compiled.join(",\n") + '};',
+      manifest: manifest
     });
   });
 };
